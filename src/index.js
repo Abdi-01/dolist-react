62779e1d95e80c88688d2dc504b576294915319f
Abdi-01 <abdialghi@gmail.com>
2022-01-11 19:54:27 +0700

    DL001: set up project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import { Reducers } from './redux/reducers'
+import ReduxThunk from 'redux-thunk'
+
+const globalStore = createStore(Reducers, {}, applyMiddleware(ReduxThunk))
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={globalStore}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+  ,
   document.getElementById('root')
 );
 
---
+++ src/page/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const HomePage = (props) => {
+
+    return (
+        <div>
+            <h1>Home Page Do List</h1>
+        </div>
+    )
+}
+
+export default HomePage;
