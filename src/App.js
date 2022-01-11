80d4354aab4b44062d4bfbd766664d11e1314922
irfansyahph <irfansyahputrahadiyat@gmail.com>
2022-01-11 21:50:19 +0700

    DL-login-001 : menyiapkan input login form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './page/Home';
+import LoginPage from './page/LoginPage';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </div>
   );
