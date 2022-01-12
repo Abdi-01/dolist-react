000bd9c64532c3e660c0093cc31cf7f0481e401a
hardilee7 <hardilee7@gmail.com>
2022-01-12 22:10:54 +0700

    DL-Register-004 : show hide password + console log

--- src/page/RegisterPage.js
+++ src/page/RegisterPage.js
@@ -1,11 +1,21 @@
 import React from "react";
 import imageRegis from '../register.png'
+import { BsFillEyeFill, BsFillEyeSlashFill } from "react-icons/bs";
 
 class RegisterPage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            pwType: "password",
+            pwVisible: <BsFillEyeFill />
+        }
+    }
 
+    btnShowHide = () => {
+        if (this.state.pwType == "password") {
+            this.setState({ pwType: "text", pwVisible: <BsFillEyeSlashFill /> })
+        } else {
+            this.setState({ pwType: "password", pwVisible: <BsFillEyeFill /> })
         }
     }
 
@@ -14,12 +24,13 @@ class RegisterPage extends React.Component {
         let email = this.refs.email.value
         let password = this.refs.password.value
         let confPassword = this.refs.confpassword.value
-        
+
         if (username == "" || email == "" || password == "" || confPassword == "") {
             alert(`Fill in registration form.`)
         } else {
             if (password == confPassword) {
                 if (email.includes("@")) {
+                    console.log(username, email, password, confPassword)
                     alert(`Your account has been succesfully created.`)
                 } else {
                     alert(`Please enter the correct email.`)
@@ -28,7 +39,7 @@ class RegisterPage extends React.Component {
                 alert(`Match your password.`)
             }
         }
-    
+
 
     }
 
@@ -52,13 +63,19 @@ class RegisterPage extends React.Component {
                         <div className="form-group py-3">
                             <label style={{ width: "100%", textAlign: "left" }}>Password</label>
                             <div className="input-group">
-                                <input type="password" className="form-control" ref="password" />
+                                <input type={this.state.pwType} className="form-control" ref="password" />
+                                <div className="input-group-append" >
+                                    <span className="input-group-text" style={{ cursor: "pointer", height: "100%" }} onClick={this.btnShowHide}>{this.state.pwVisible}</span>
+                                </div>
                             </div>
                         </div>
                         <div className="form-group py-3">
                             <label style={{ width: "100%", textAlign: "left" }}>Confirm Password</label>
                             <div className="input-group">
-                                <input type="password" className="form-control" ref="confpassword" />
+                                <input type={this.state.pwType} className="form-control" ref="confpassword" />
+                                <div className="input-group-append" >
+                                    <span className="input-group-text" style={{ cursor: "pointer", height: "100%" }} onClick={this.btnShowHide}>{this.state.pwVisible}</span>
+                                </div>
                             </div>
                         </div>
                         <div>
