217d045d19655f6b6ff2fe860b5c934f19b3f44b
hardilee7 <hardilee7@gmail.com>
2022-01-12 20:41:18 +0700

    DL-register-002 : membuat fungsi input dan button

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './page/Home';
+import RegisterPage from './page/RegisterPage';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<HomePage />} exact />
-        <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </div>
--- src/page/RegisterPage.js
+++ src/page/RegisterPage.js
@@ -1,21 +1,51 @@
 import React from "react";
-import axios from "axios";
-import { Redirect } from 'react-router-dom'
+import imageRegis from '../register.png'
 
 class RegisterPage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            redirect: false
+
         }
     }
 
     render() {
-        return <Redirect to="/" />
+        return (
+            <div className="container pt-5">
+                <h1>Register Page</h1>
+                <div className="row">
+                    <div className="col-6">
+                        <img src={imageRegis} alt="register" style={{ width: "100%" }} />
+                    </div>
+                    <div className="col-6">
+                        <div className="form-group py-3">
+                            <label style={{ width: "100%", textAlign: "left" }}>Username</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="form-group py-3">
+                            <label style={{ width: "100%", textAlign: "left" }}>Email</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="form-group py-3">
+                            <label style={{ width: "100%", textAlign: "left" }}>Password</label>
+                            <div className="input-group">
+                                <input type="password" className="form-control" />
+                            </div>
+                        </div>
+                        <div className="form-group py-3">
+                            <label style={{ width: "100%", textAlign: "left" }}>Confirm Password</label>
+                            <div className="input-group">
+                                <input type="password" className="form-control" />
+                            </div>
+                        </div>
+                        <div>
+                            <button>Create Your Account</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
     }
-    return (
-
-    )
 }
 
-export default RegisterPage
+export default RegisterPage;
